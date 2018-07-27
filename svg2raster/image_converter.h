ba1acedfe2bf88846c7bf4d0482f63cc7589d671
Dima Rusyy <dima.ru.com@gmail.com>
2018-07-27 17:26:21 +0300

    - added ipc_cache [work in progress] - implemented file conversion

--- svg2raster/image_converter.h
+++ svg2raster/image_converter.h
@@ -20,24 +20,23 @@ namespace converter
 	{
 		std::string _converter;
 		std::string _scale_type;
-		uint32_t _x;
-		uint32_t _y;
+		uint32_t _scale_w;
+		uint32_t _scale_h;
 	};
 
-	static auto r_parser = x3::rule<struct _, converter::settings>{ "processor_cfg" };
+	static auto r_parser = x3::rule<struct _, converter::settings>{ "settings" };
 	static auto r_parser_def = +x3::char_("a-z") >> '&' >> +x3::char_("a-z") >> '=' >> x3::uint_ >> 'x' >> x3::uint_;
 
 	BOOST_SPIRIT_DEFINE(r_parser)
 };
 
-BOOST_FUSION_ADAPT_STRUCT(converter::settings, (std::string, _converter)(std::string, _scale_type)(uint32_t, _x)(uint32_t, _y))
+BOOST_FUSION_ADAPT_STRUCT(converter::settings, (std::string, _converter)(std::string, _scale_type)(uint32_t, _scale_w)(uint32_t, _scale_h))
 
 //////////////////////////////////////////////////////////////////////////
 
 struct image_converter 
 {
-	image_converter(const std::string& img_path, const std::string& query_str)
-		: _img_path(img_path)
+	image_converter(const std::string& query_str)
 	{
 		namespace x3 = boost::spirit::x3;
 
@@ -51,13 +50,21 @@ struct image_converter
 		}
 	}
 
-	void process()
+	void apply(const std::string& img_path, const std::string& img_out_path)
 	{
+		Magick::Image image;
 
+		// Determine if Warning exceptions are thrown.
+		// Use is optional.  Set to true to block Warning exceptions.
+		image.quiet(false);
+		image.read(img_path);
+		image.scale(Magick::Geometry(_cfg._scale_w, _cfg._scale_h));
+
+		//output
+		image.write(img_out_path);
 	}
 
 private:
-	std::string _img_path;
 	converter::settings _cfg;
 };
 
--- svg2raster/ipc_cache.h
+++ svg2raster/ipc_cache.h
@@ -7,4 +7,21 @@
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/sync/named_recursive_mutex.hpp>
 
+template <typename T>
+using ipc_shared_alloc_t = boost::interprocess::allocator<T, boost::interprocess::managed_shared_memory::segment_manager>;
+
+using ipc_string_t = boost::interprocess::basic_string<char, std::char_traits<char>, ipc_shared_alloc_t<char>> ;
+
+using ipc_string_allocator_t = ipc_shared_alloc_t<ipc_string_t>;
+
+template <typename T>
+using ipc_pair_allocator_t = ipc_shared_alloc_t<std::pair<ipc_string_t const, T>>;
+
+template <typename T, typename V>
+using ipc_shared_map_t = boost::interprocess::map<T, V, std::less<T>, ipc_shared_alloc_t<std::pair<T const, V>>>;
+
+template <typename T>
+using ipc_map_t = ipc_shared_map_t<ipc_string_t, T>;
+
+
 
